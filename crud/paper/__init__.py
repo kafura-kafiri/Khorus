10b2176ed81c85d38a3343e2e3b57e740012f155
pouria sharifi <kafura.kafiri@gmail.com>
2018-01-24 21:16:46 +0330

    i'm your majesty

--- crud/paper/__init__.py
+++
@@ -1,8 +0,0 @@
-from Choori.mongo import Mongo
-from config import paper as config
-from sanic import Blueprint
-from crud import prime
-
-papers = Mongo(config['collection']['name'], config['collection']['indexes'])
-bp = Blueprint(config['name'], url_prefix=config['path'])
-prime(bp, papers)
